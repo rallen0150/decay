630a5087923357a8b58c4bc53f2f40b2c50b83b9
rallen0150 <rallen0150@gmail.com>
2019-03-05 19:02:08 -0500

    heroku always awake

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ res.sendFile(path.join(__dirname+'/dist/radioactive/index.html'));
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
-// var http = require("http");
-// setInterval(function() {
-//     http.get("http://smartsparrow-decay.herokuapp.com");
-// }, 300000);
+var http = require("http");
+setInterval(function() {
+    http.get("http://smartsparrow-decay.herokuapp.com");
+}, 300000);
